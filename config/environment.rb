3e5516551cf997375b27e774b3b490053cc59df6
Jason <jason@sisk.org>
2010-05-21 21:40:31 -0400

    added gems and froze rails

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
+  config.gem "authlogic"
+  config.gem "acl9"
+  config.gem "formtastic"
+  config.gem "paperclip"
+  config.gem "inherited_resources"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
