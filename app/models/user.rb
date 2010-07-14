f5e46d823ce535a5b92e4c7cf9d27146ebeeed23
Jason <jason@sisk.org>
2010-07-14 13:52:47 -0400

    hamlized remaining devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name_last, :name_first
 
   validates_presence_of :email, :message => "can't be blank"
   validates_presence_of :name_first, :message => "can't be blank"
