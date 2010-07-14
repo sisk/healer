0ef64acf49491a3906e7af805e9e2b9390e56d88
Jason <jason@sisk.org>
2010-07-14 13:53:17 -0400

    made dev mode use mailtrap; refined devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,15 @@ Healer::Application.configure do
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :domain => "derp.com",
+    :address => "localhost",
+    :port => 2525,
+  }
+
 end
