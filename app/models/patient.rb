f5e5c5984c2dadb717413c5c6d5ac6511d805d49
Jason <jason@sisk.org>
2010-07-15 01:04:40 -0400

    added ability for photo upload on patients; fixed email wonk on forms

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,13 +3,19 @@ class Patient < ActiveRecord::Base
   validates_presence_of :name_last, :message => "can't be blank"
   validates_inclusion_of :male, :in => [true, false]
 
-  validates :email, :presence => true, 
-                    :length => {:minimum => 3, :maximum => 254},
+  validates :email, :length => {:minimum => 3, :maximum => 254},
                     :uniqueness => true,
-                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+                    :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i},
+                    :allow_blank => true,
+                    :allow_nil => true
                     # :email => true
   
   has_many :patient_interactions
+  # Paperclip
+  has_attached_file :photo,
+    :styles => {
+      :thumb=> "100x100#",
+      :small  => "150x150>" }
   
   def to_s(*args)
     name(*args)
