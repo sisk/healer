022a10f50f747c67095d7ae813130706a49039bf
sisk <jasonmsisk@gmail.com>
2010-07-21 22:45:25 -0400

    Modified trips and global nav to exclude unauthorized actions

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,6 +1,9 @@
 authorization do
   role :guest do
-    has_permission_on :trips, :to => [:index, :show]
+    has_permission_on :trips, :to => [:show]
+    # has_permission_on :users, :to => [:show, :edit] do
+    #   if_attribute :user => is { user }
+    # end
   end
   role :admin do
     has_permission_on [:trips, :users, :patients], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
