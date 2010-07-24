e38d2c9cf2a169e5629fb2204af6165f91136ceb
sisk <jasonmsisk@gmail.com>
2010-07-24 07:35:15 -0400

    skeletal controller framework for something called 'staff.' might not use this.

--- app/controllers/staff_controller.rb
+++ app/controllers/staff_controller.rb
@@ -1,6 +1,6 @@
 class StaffController < InheritedResources::Base
   defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
-  filter_resource_access
+  # filter_resource_access
 
   before_filter :authenticate_user!
 
