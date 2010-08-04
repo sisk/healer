5f25c7d95c5e22c6378aacfb81ecb00d0d411653
sisk <jasonmsisk@gmail.com>
2010-08-03 21:51:47 -0400

    more auth rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -6,12 +6,18 @@ authorization do
     end
   end
   role :admin do
-    has_permission_on [:trips, :users, :patients, :body_parts], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
-  end
-  role :doctor do
-    has_permission_on [:trips, :patients], :to => [:index, :show, :edit, :update]
+    has_permission_on [:trips, :users, :patients, :body_parts, :diagnoses], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
   end
   role :nurse do
-    has_permission_on [:trips, :patients], :to => [:index, :show, :edit, :update]
+    has_permission_on [:trips], :to => [:index, :show]
+    #has_permission_on [:diagnoses], :to => [:index, :show, :edit, :update]
+    has_permission_on [:patients], :to => [:index, :show, :edit, :update] do
+      has_permission_on :diagnoses, :to => [:index, :show, :create, :update, :edit, :destroy]
+    end
+  end
+  role :doctor do
+    include :nurse
+    # has_permission_on [:trips, :patients], :to => [:index, :show, :edit, :update]
+    has_permission_on [:diagnoses], :to => [:index, :show, :edit, :update]
   end
 end
