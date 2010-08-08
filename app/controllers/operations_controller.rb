08a2c9a14b5f4301425b3c779b029e0c1b5d7506
sisk <jasonmsisk@gmail.com>
2010-08-08 10:31:56 -0400

    Augmented resource build in operations controller to determine diagnosis from param if it exists

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -9,10 +9,15 @@ class OperationsController < InheritedResources::Base
   
   private
   
-  # def build_resource 
-  #    super 
-  #    @operation.build_implant(params[:implant]) 
-  #    @operation
-  # end 
+  def build_resource 
+     super
+     @operation.diagnosis ||= diagnosis_by_param
+     @operation.body_part = @operation.diagnosis.try(:body_part)
+     @operation
+  end
+  
+  def diagnosis_by_param
+    Diagnosis.find(params[:diagnosis_id]) if params[:diagnosis_id].present?
+  end
   
 end
