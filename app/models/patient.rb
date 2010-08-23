ed47e6e22a954f9edcebf51282b79d01b1b74e47
sisk <jasonmsisk@gmail.com>
2010-08-23 00:04:50 -0400

    Moved photo repository for Patients to S3

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -20,7 +20,15 @@ class Patient < ActiveRecord::Base
   has_attached_file :photo,
     :styles => {
       :thumb=> "100x100#",
-      :small  => "150x150>" }
+      :small  => "150x150>" },
+    :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    },
+    :bucket => ENV['S3_BUCKET'],
+    :path => ":attachment/:id"
+
   
   def to_s(*args)
     name(*args)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ActiveRecord::Base
-
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
   # acts_as_authorization_subject
 
   # Include default devise modules. Others available are:
