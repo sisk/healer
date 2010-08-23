1d00fd951af60fdb53356824eb24e059af7c366f
sisk <jasonmsisk@gmail.com>
2010-08-22 21:54:11 -0400

    Added confirmation step to user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
-  # acts_as_authorization_subject
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation
+  # acts_as_authorization_subject
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :name_last, :name_first
