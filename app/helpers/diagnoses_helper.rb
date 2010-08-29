39b211cd824e4aa22ad85b00c7f7049c3395f0b3
sisk <jasonmsisk@gmail.com>
2010-08-29 13:40:10 -0400

    A couple helper methods for diagnosis

--- app/helpers/diagnoses_helper.rb
+++ app/helpers/diagnoses_helper.rb
@@ -1,5 +1,11 @@
 module DiagnosesHelper
+
   def date_assessed(diagnosis)
-    diagnosis.assessed_date.kind_of?(Date) ? "assessed #{diagnosis.assessed_date}" : "unknown assessment date"
+    diagnosis.assessed_date.kind_of?(Date) ? "#{diagnosis.assessed_date}" : "Unknown"
   end
+
+  def severity(diagnosis)
+    Diagnosis.severity_table[diagnosis.severity]
+  end
+
 end
--- spec/helpers/diagnoses_helper_spec.rb
+++ spec/helpers/diagnoses_helper_spec.rb
@@ -13,10 +13,18 @@ require 'spec_helper'
 describe DiagnosesHelper, "#date_assessed" do
   it "outputs a formatted date string if date is known" do
     @diagnosis = Diagnosis.new(:assessed_date => "2010-06-07".to_date)
-    helper.date_assessed(@diagnosis).should == "assessed 2010-06-07"
+    helper.date_assessed(@diagnosis).should == "2010-06-07"
   end
   it "outputs unknown string if date is not known" do
     @diagnosis = Diagnosis.new(:assessed_date => nil)
-    helper.date_assessed(@diagnosis).should == "unknown assessment date"
+    helper.date_assessed(@diagnosis).should == "Unknown"
+  end
+end
+
+describe DiagnosesHelper, "#severity" do
+  it "returns the proper corresponding value from severity_table" do
+    Diagnosis.stub(:severity_table).and_return({ 0 => "Derp" })
+    @diagnosis = Diagnosis.new(:severity => 0)
+    helper.severity(@diagnosis).should == "Derp"
   end
 end
