f622cb814e7f6e54df67baac7ec4d3f6c29427ac
sisk <jasonmsisk@gmail.com>
2010-08-29 13:40:43 -0400

    patient helper method for Xray; made Xray output smarter with a built-in to_s

--- app/models/xray.rb
+++ app/models/xray.rb
@@ -1,12 +1,14 @@
 class Xray < ActiveRecord::Base
   
   belongs_to :diagnosis
+  belongs_to :operation
 
   # Paperclip
   has_attached_file :photo,
     :styles => {
+      :tiny=> "60x60#",
       :thumb=> "100x100#",
-      :small  => "150x150>" },
+      :small  => "200x200>" },
     :storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
     :s3_credentials => {
       :access_key_id => ENV['S3_KEY'],
@@ -17,5 +19,15 @@ class Xray < ActiveRecord::Base
     :url => ENV['S3_BUCKET'] ? "xrays/:attachment/:id/:style/:basename.:extension" : "/system/xrays/:attachment/:id/:style/:basename.:extension"
 
     validates_attachment_presence :photo
+
+  def patient
+    diagnosis.try(:patient) || operation.try(:patient)
+  end
+
+  def to_s
+    str = "X-ray: #{patient.to_s}"
+    str << " - " + date_time.to_s if date_time.present?
+    return str
+  end
   
 end
--- spec/models/xray_spec.rb
+++ spec/models/xray_spec.rb
@@ -9,5 +9,40 @@ describe Xray do
 
   # should_validate_presence_of :photo
   should_belong_to :diagnosis
+  should_belong_to :operation
 
+end
+
+describe Xray, "#patient" do
+  before(:each) do
+    @xray = Xray.new()
+  end
+  it "returns the patient attached to diagnosis if set" do
+    patient = mock_model(Patient)
+    @xray.diagnosis = mock_model(Diagnosis, :patient => patient)
+    @xray.patient.should == patient
+  end
+  it "returns the operation patient if set" do
+    patient = mock_model(Patient)
+    @xray.operation = mock_model(Operation, :patient => patient)
+    @xray.patient.should == patient
+  end
+  it "returns nil by default" do
+    @xray.patient.should == nil
+  end
+end
+
+describe Xray, "#to_s" do
+  before(:each) do
+    @xray = Xray.new
+    @xray.stub_chain(:patient, :to_s).and_return("Derp")
+  end
+  it "returns expected string if date_time is not set" do
+    @xray.to_s.should == "X-ray: Derp"
+  end
+  it "returns expected string if date_time is set" do
+    the_time = "2010-07-10 5:00pm".to_time
+    @xray.date_time = the_time
+    @xray.to_s.should == "X-ray: Derp - #{the_time.to_s}"
+  end
 end
