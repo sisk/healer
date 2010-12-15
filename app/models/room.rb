af3590cb3e490e22e1a780d4c39fac38c22e47f6
sisk <jasonmsisk@gmail.com>
2010-12-15 15:03:32 -0500

    Add ability to delete a room; nullify its associated records on delete

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ActiveRecord::Base
   belongs_to :facility
   validates_presence_of :facility
-  has_many :operations
-  has_many :registrations
+  has_many :operations, :dependent => :nullify
+  has_many :registrations, :dependent => :nullify
   
   def to_s
     title
