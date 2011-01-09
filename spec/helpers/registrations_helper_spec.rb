2221c62f2377bd968ef80d4fb82b34964d73d900
sisk <jasonmsisk@gmail.com>
2011-01-09 16:34:21 -0500

    Tweak body_part_list helper to output bilateral where applicable

--- spec/helpers/registrations_helper_spec.rb
+++ spec/helpers/registrations_helper_spec.rb
@@ -1,15 +1,35 @@
 require 'spec_helper'
 
 describe RegistrationsHelper, "#body_part_list" do
+  before(:each) do
+    @left_knee = stub_model(BodyPart, :to_s => "Knee (L)", :name => "Knee")
+    @right_knee = stub_model(BodyPart, :to_s => "Knee (R)", :name => "Knee")
+    @right_hip = stub_model(BodyPart, :to_s => "Hip (R)", :name => "Hip")
+  end
   it "outputs a formatted date string of body parts for its diagnoses" do
     registration = Registration.new(:diagnoses => [
-      stub_model(Diagnosis, :body_part => stub_model(BodyPart, :to_s => "Knee (L)")),
-      stub_model(Diagnosis, :body_part => stub_model(BodyPart, :to_s => "Knee (R)")),
+      stub_model(Diagnosis, :body_part => @left_knee),
+      stub_model(Diagnosis, :body_part => @right_knee),
     ])
     helper.body_part_list(registration).should == "Knee (L), Knee (R)"
   end
   it "outputs empty string if no diagnoses" do
     registration = Registration.new(:diagnoses => [])
     helper.body_part_list(registration).should == ""
+  end
+  it "outputs bilateral if registration is likely bilateral and body parts are the same" do
+    registration = Registration.new(:likely_bilateral => true, :diagnoses => [
+      stub_model(Diagnosis, :body_part => @left_knee),
+      stub_model(Diagnosis, :body_part => @right_knee),
+    ])
+    helper.body_part_list(registration).should == "Knee (Bilateral)"
+  end
+  it "separates bilateral from non-bilateral parts" do
+    registration = Registration.new(:likely_bilateral => true, :diagnoses => [
+      stub_model(Diagnosis, :body_part => @left_knee),
+      stub_model(Diagnosis, :body_part => @right_knee),
+      stub_model(Diagnosis, :body_part => @right_hip),
+    ])
+    helper.body_part_list(registration).should == "Knee (Bilateral), Hip (R)"
   end
 end
