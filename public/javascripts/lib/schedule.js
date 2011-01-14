96235c5725c78a06d500d359a73fce70f78fae0a
sisk <jasonmsisk@gmail.com>
2011-01-13 21:41:28 -0500

    Refactored some JS

--- public/javascripts/lib/schedule.js
+++ public/javascripts/lib/schedule.js
@@ -18,28 +18,13 @@ $(document).ready(function() {
       //alert("sender:" + ui.sender.attr("id"));
     },
     update: function(event, ui) {
-      var enclosure, room_id, day_num, params, template, url, item_id;
+      var item_id;
       item_id = ui.item.attr("id");
-      enclosure = $(this).closest(".room");
-      
-      room_id = $(this).closest(".room").attr("class").match(/room_(\d+)/)[1];
-      day_num = $(this).closest(".room").attr("class").match(/day_(\d+)/)[1];
-      
-      params = enclosure.sortable("serialize") + "&room_id=" + room_id + "&day=" + day_num;
-      template = Handlebars.compile($("script[name=registration_scheduled_template]").html());
-      //url = "/trips/#{@trip.id}/schedule/sort_room.json";
-      url = "/trips/1/schedule/sort_room.json";
-
-      // alert("updating room: " + room_id + ", day: " + day_num);
-      enclosure.html('<img class="ajax_loader" src="/images/ajax-loader.gif" />');
-
-      $.getJSON(url, params, function(data) {
-        enclosure.html(template(data));
-        if (ui.sender) {
-          // alert("there was a sender:" + ui.sender.attr("id"));
-          // stop moved items from re-rendering in the source column
-        }
-      });
+      updateRoom($(this));
+      if (ui.sender) {
+        // stop moved items from re-rendering in the source column
+        ui.sender.find("#"+ui.item.attr("id")).remove();
+      }
       $("#"+item_id).flash();
     }
   }).disableSelection();
@@ -54,28 +39,15 @@ $(document).ready(function() {
     receive: function(event, ui) {
       // stop moved items from re-rendering in the source column
       ui.sender.find("#"+ui.item.attr("id")).remove();
-      //alert("sender:" + ui.sender.attr("id"));
     },
     update: function(event, ui) {
-      var enclosure, params, template, url, item_id;
+      var item_id;
       item_id = ui.item.attr("id");
-      enclosure = $(this).closest(".registrations");
-      params = enclosure.sortable("serialize");
-
-      // alert("updating unscheduled");
-      enclosure.html('<img class="ajax_loader" src="/images/ajax-loader.gif" />');
-
-      template = Handlebars.compile($("script[name=registration_unscheduled_template]").html());
-      //url = "/trips/#{@trip.id}/schedule/sort_unscheduled.json";
-      url = "/trips/1/schedule/sort_unscheduled.json";
-      $.getJSON(url, params, function(data) {
-        enclosure.html(template(data));
-        if (ui.sender) {
-          // stop moved items from re-rendering in the source column
-          ui.sender.find("#"+ui.item.attr("id")).remove();
-        }
-      });
-
+      updateUnscheduled();
+      if (ui.sender) {
+        // stop moved items from re-rendering in the source column
+        ui.sender.find("#"+ui.item.attr("id")).remove();
+      }
       $("#"+item_id).flash();
     }
   }).disableSelection();
@@ -87,6 +59,12 @@ $(document).ready(function() {
     $("#source .registration:contains('"+query+"')").show();
     //$("#generated_fld").val($(this).val()).keyup();
     // $("div:contains('John')")
+  });
+  
+  $("#schedule .unschedule").live("click", function() {
+    var registration;
+    registration = $(this).closest(".registration");
+    alert(registration.attr("id"));
   });
      
   /*
@@ -107,3 +85,35 @@ $(document).ready(function() {
   });
   */
 });
+
+function updateUnscheduled() {
+  var params, template, url;
+  params = $("#unscheduled").sortable("serialize");
+
+  $("#unscheduled").html('<img class="ajax_loader" src="/images/ajax-loader.gif" />');
+
+  template = Handlebars.compile($("script[name=registration_unscheduled_template]").html());
+  url = "/trips/1/schedule/sort_unscheduled.json";
+  $.getJSON(url, params, function(data) {
+    $("#unscheduled").html(template(data));
+  });
+}
+
+function updateRoom(element) {
+  var enclosure, room_id, day_num, params, template, url;
+  enclosure = element.closest(".room");
+  
+  room_id = element.closest(".room").attr("class").match(/room_(\d+)/)[1];
+  day_num = element.closest(".room").attr("class").match(/day_(\d+)/)[1];
+
+  url = "/trips/1/schedule/sort_room.json";
+  
+  params = enclosure.sortable("serialize") + "&room_id=" + room_id + "&day=" + day_num;
+  template = Handlebars.compile($("script[name=registration_scheduled_template]").html());
+
+  enclosure.html('<img class="ajax_loader" src="/images/ajax-loader.gif" />');
+
+  $.getJSON(url, params, function(data) {
+    enclosure.html(template(data));
+  });
+}
