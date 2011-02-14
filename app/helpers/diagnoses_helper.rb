2e8b46b4347a6ae390f0f9543cad40ae3c48b973
sisk <jasonmsisk@gmail.com>
2011-02-13 19:16:57 -0500

    Diagnosis view titling changes

--- app/helpers/diagnoses_helper.rb
+++ app/helpers/diagnoses_helper.rb
@@ -7,5 +7,10 @@ module DiagnosesHelper
   def severity(diagnosis)
     Diagnosis.severity_table[diagnosis.severity]
   end
+  
+  def formatted_title(diagnosis)
+    str = "<h4>#{diagnosis.disease.to_s}</h4>"
+    return diagnosis.body_part.present? ? "<h3>#{diagnosis.body_part.to_s}</h3>" + str : str
+  end
 
 end
--- spec/helpers/diagnoses_helper_spec.rb
+++ spec/helpers/diagnoses_helper_spec.rb
@@ -18,3 +18,16 @@ describe DiagnosesHelper, "#severity" do
     helper.severity(@diagnosis).should == "Derp"
   end
 end
+
+describe DiagnosesHelper, "#formatted_title" do
+  before(:each) do
+    @diagnosis = Diagnosis.new(:disease => stub_model(Disease, :to_s => "Derp"), :body_part => stub_model(BodyPart, :to_s => "Knee"))
+  end
+  it "contains the h3 and h4 if body part is set" do
+    helper.formatted_title(@diagnosis).should == "<h3>Knee</h3><h4>Derp</h4>"
+  end
+  it "contains h4 only if body part is not set" do
+    @diagnosis.body_part = nil
+    helper.formatted_title(@diagnosis).should == "<h4>Derp</h4>"
+  end
+end
