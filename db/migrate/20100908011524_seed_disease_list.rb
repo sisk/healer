4d95aa123e8f6ba385b4e5e212989c835ed5f2c2
sisk <jasonmsisk@gmail.com>
2011-06-14 13:50:17 -0400

    Fix migration that was preventing a reset

--- db/migrate/20100908011524_seed_disease_list.rb
+++ db/migrate/20100908011524_seed_disease_list.rb
@@ -2,6 +2,12 @@ class SeedDiseaseList < ActiveRecord::Migration
   
   @diseases = ["osteoarthritis (primary)","osteoarthritis (secondary)","osteonecrosis","rheumatoid arthritis","loose joint replacement","infected joint","fracture of neck of femur","dislocation of hip","congenital dislocation of hip","non union femoral neck","painful hemi arthroplasty","osteolysis/wear","frieburg infarction (osteonecrosis metatarsal head)","residual clubfoot","residual congenital vertical talus","equinous contracture","equinovarus deformity","flatfoot deformity","charcot deformity","foot drop","subtalar arthritis","bunion","hammer toe","claw toe","ankle arthritis","hindfoot arthritis","subtalar arthritis","midfoot arthritis","1st MTP arthritis","fracture nonunion"]
   
+  class Disease < ActiveRecord::Base
+    def to_s
+      name
+    end
+  end
+  
   def self.up
     Disease.destroy_all
     i = 0
