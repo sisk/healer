8540d820e32e6881ef2ba65258837a79c1378137
sisk <jasonmsisk@gmail.com>
2011-06-30 17:49:49 -0400

    Tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   filter_resource_access
 
-  before_filter :set_selectable_roles, :except => [:create, :destroy]
+  before_filter :set_selectable_roles, :except => [:destroy]
   
   def index
     index! do |format|
