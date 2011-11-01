f300c65b9676a5f30d8a611f7b70841b42c08500
sisk <jasonmsisk@gmail.com>
2011-11-01 01:01:26 -0400

    Errors in mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,8 +1,11 @@
 class Mailer < ActionMailer::Base
-  include ActionController::UrlWriter
-  
+  include ActionDispatch::Routing::UrlFor
+  # include ActionController::PolymorphicRoutes
+  include Rails.application.routes.url_helpers
+
+  default_url_options[:host] = 'healer.ws'
   default :from => "healer.app@gmail.com"
-  
+
   def case_added(patient_case, recipients = [])
     @patient_case = patient_case
     mail(:to => recipients)
