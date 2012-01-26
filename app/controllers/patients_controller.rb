afe2270baafa3895f4461873959f059d8305e239
sisk <jasonmsisk@gmail.com>
2012-01-25 22:20:56 -0500

    Re-implement medical history display for patient records

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,15 +1,15 @@
 class PatientsController < ApplicationController
   inherit_resources
-  
+
   before_filter :authenticate_user!
   filter_resource_access
-  
+
   def create
     create! { patients_path }
   end
 
   def update
-    update! { resource_path }
+    update! { edit_resource_path }
   end
 
 private
@@ -31,5 +31,5 @@ protected
     end
     @patients ||= start.paginate(:page => params[:page], :per_page => 5)
   end
-  
+
 end
