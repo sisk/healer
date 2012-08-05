184cdac7ce078a0fcf4d4337a232c503268b3d19
Jason Sisk <jsisk@groupon.com>
2012-08-04 23:29:16 -0500

    Turn of DB initialization for asset compile

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Healer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.initialize_on_precompile = false
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
