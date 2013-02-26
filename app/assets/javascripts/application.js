c274ca2e08ebafa799dc641c90d38c5c3bee54f2
Brandon Ferguson <bnferguson@gmail.com>
2013-02-26 09:58:30 -0600

    Remember open cases using local storage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
+try {
+  'localStorage' in window && window['localStorage'] !== null;
+} catch (e) {
+  localStorage = {};
+}
+
 jQuery.ajaxSetup({
   'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 });
@@ -90,14 +96,32 @@ jQuery(function ($) {
 
 (function($){
   $.fn.collapsible = function() {
+    expandedItems = JSON.parse(localStorage.getItem("expandedItems")) || {};
+
+    // Reopen left open tabs.
+    for (var prop in expandedItems) {
+      if(expandedItems.hasOwnProperty(prop)) {
+        $("#" + prop).removeClass("collapsed");
+        $("#" + prop).find(".supplement").toggle();
+      }
+    }
+
     var toggle_trigger = $(this).find(".basic");
-    toggle_trigger.bind('click', function(event) {
+      toggle_trigger.bind('click', function(event) {
+
+      expandedId = event.currentTarget.parentNode.id;
       if ($(this).parent().hasClass("collapsed")) {
         $(this).parent().removeClass("collapsed");
+        expandedItems[expandedId] = true;
       } else {
         $(this).parent().addClass("collapsed");
+        delete expandedItems[expandedId];
       }
       $(this).siblings(".supplement").toggle();
+
+
+      localStorage.setItem("expandedItems", JSON.stringify(expandedItems));
+
     });
     return this;
   }
