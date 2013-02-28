f29e220c879524d2f2f1a9c2a192527113eead47
Brandon Ferguson <bnferguson@gmail.com>
2013-02-28 08:08:49 -0600

    Fixing busted migration. Index already exists

--- db/migrate/20130228123641_augment_adverse_events.rb
+++ db/migrate/20130228123641_augment_adverse_events.rb
@@ -3,6 +3,5 @@ class AugmentAdverseEvents < ActiveRecord::Migration
     add_column :adverse_events, :date_of_occurrence, :date
     change_column :adverse_events, :reason, :string, :null => true
     rename_column :adverse_events, :reason, :event_type
-    add_index :adverse_events, :case_id
   end
 end
