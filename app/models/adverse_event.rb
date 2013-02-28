092df6fd6c1adfa3e67a7d37c405a08690a400a7
sisk <jason@sisk.org>
2013-02-28 07:10:42 -0600

    Revert "Revert "Modify adverse event form""
    This reverts commit ab9818f703f79ae86a9aea6c9ac51269f0702cd0.

--- app/models/adverse_event.rb
+++ app/models/adverse_event.rb
@@ -4,9 +4,21 @@ class AdverseEvent < ActiveRecord::Base
   validates_presence_of :patient
   belongs_to :entered_by, :class_name => "User", :foreign_key => "entered_by_id"
 
+  def self.event_types
+    [
+      "Instability",
+      "Phlebitis",
+      "Pulmonary embolus",
+      "Infection",
+      "Dislocation",
+      "Fracture",
+      "Non-Union and Loose",
+      "Other"
+    ]
+  end
+
   def to_s
     "Event logged #{created_at}"
   end
-
 
 end
