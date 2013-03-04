d10c8b8d0ae25ab001ce6b6f17f5655fb1bd1b00
sisk <jason@sisk.org>
2013-03-03 21:23:59 -0600

    Tweak migration

--- db/migrate/20120226184502_remove_password_requirements_on_users.rb
+++ db/migrate/20120226184502_remove_password_requirements_on_users.rb
@@ -1,11 +1,11 @@
 class RemovePasswordRequirementsOnUsers < ActiveRecord::Migration
   def up
     change_column :users, :encrypted_password, :string, :null => true
-    change_column :users, :password_salt, :string, :null => true
+    # change_column :users, :password_salt, :string, :null => true
   end
 
   def down
     change_column :users, :encrypted_password, :string, :null => false
-    change_column :users, :password_salt, :string, :null => false
+    # change_column :users, :password_salt, :string, :null => false
   end
 end
