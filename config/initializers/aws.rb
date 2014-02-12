25af2db9182ccf8c39c57cc15f33764fa2c327d6
Jason Sisk <jsisk@groupon.com>
2014-02-11 22:19:49 -0600

    Protect against absent aws.yml

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-config_vals = YAML.load_file("config/aws.yml")[Rails.env] || {}
+config_vals = File.exists?("config/aws.yml") ? YAML.load_file("config/aws.yml")[Rails.env] : {}
 AWS.config(
   access_key_id: "#{config_vals["AWS_ACCESS_KEY_ID"]}",
   secret_access_key: "#{config_vals["AWS_SECRET_ACCESS_KEY"]}",
