459fd0ede62f2adab363dc8c6ee619163dba7130
Jason Sisk <jsisk@groupon.com>
2014-02-16 11:33:07 -0600

    Add more methods to patient case decorator

--- app/decorators/v1/patient_case_decorator.rb
+++ app/decorators/v1/patient_case_decorator.rb
@@ -19,6 +19,40 @@ class V1::PatientCaseDecorator < V1::BaseDecorator
     [anatomy_short.titlecase, side_abbr].compact.join(" ")
   end
 
+  def patient
+    model.patient.name_full
+  end
+
+  def procedure
+    return nil unless model.operation
+    return unspecified_operation_message unless procedure = model.operation.procedure
+
+    if I18n.locale == :es
+      procedure.name_es || procedure.name_en
+    else
+      procedure.name_en
+    end
+  end
+
+  def operation_date
+    return nil unless model.operation
+    return unspecified_date_message unless date = model.operation.date
+
+    if I18n.locale == :es
+      date.to_formatted_s(:short_spanish)
+    else
+      date.to_formatted_s(:short_english)
+    end
+  end
+
+  def xrays(opts = {})
+    set = all_xrays
+    set = set & primary_xrays if opts[:primary]
+    set = set & pre_op_xrays if opts[:pre_op]
+    set = set & post_op_xrays if opts[:post_op]
+    set
+  end
+
 
   private ######################################################################
 
@@ -32,6 +66,30 @@ class V1::PatientCaseDecorator < V1::BaseDecorator
 
   def side_full
     I18n.t("#{model.side}")
+  end
+
+  def unspecified_operation_message
+    "[#{I18n.t("unspecified_operation")}]"
+  end
+
+  def unspecified_date_message
+    "[#{I18n.t("unspecified_date")}]"
+  end
+
+  def all_xrays
+    @all_xrays ||= model.xrays
+  end
+
+  def primary_xrays
+    @primary_xrays ||= all_xrays.select{ |x| x.primary == true }
+  end
+
+  def post_op_xrays
+    @post_op_xrays ||= all_xrays.select{ |x| x.operation_id.present? }
+  end
+
+  def pre_op_xrays
+    @pre_op_xrays ||= all_xrays.select{ |x| x.operation_id.nil? }
   end
 
 end
--- spec/v1/unit/decorators/patient_case_decorator_spec.rb
+++ spec/v1/unit/decorators/patient_case_decorator_spec.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require "spec_helper"
 require "v1/patient_case_decorator"
 
@@ -141,6 +142,184 @@ describe V1::PatientCaseDecorator do
 
         decorator.anatomy.should == "parte del cuerpo no especificada"
       end
+    end
+  end
+
+  describe "#patient" do
+    it "delegates to the patient name" do
+      decorator = V1::PatientCaseDecorator.new(
+        build(:patient_case, :patient => create(:patient, :name_full => "Merrill Ritter"))
+      )
+
+      decorator.patient.should == "Merrill Ritter"
+    end
+  end
+
+  describe "#procedure" do
+    it "is nil if no operation present" do
+      decorator = V1::PatientCaseDecorator.new(build(:patient_case))
+
+      decorator.procedure.should be_nil
+    end
+
+    context "in English" do
+      before(:each) do
+        I18n.locale = :en
+      end
+
+      it "delegates to the operation's procedure" do
+        operation = create(:operation,
+          :procedure => create(
+            :procedure, :name_en => "derp"))
+        decorator = V1::PatientCaseDecorator.new(
+          create(:patient_case, :operation => operation))
+
+        decorator.procedure.should == "derp"
+      end
+
+      it "is 'unspecified' if operation has no procedure" do
+        operation = create(:operation, :procedure => nil)
+        decorator = V1::PatientCaseDecorator.new(
+          build(:patient_case, :operation => operation)
+        )
+
+        decorator.procedure.should == "[Unspecified procedure]"
+      end
+    end
+
+    context "in Spanish" do
+      before(:each) do
+        I18n.locale = :es
+      end
+
+      it "delegates to the operation's procedure in Spanish" do
+        operation = create(:operation,
+          :procedure => create(
+            :procedure, :name_en => "derp",
+                        :name_es => "el derp"))
+        decorator = V1::PatientCaseDecorator.new(
+          create(:patient_case, :operation => operation))
+
+        decorator.procedure.should == "el derp"
+      end
+
+      it "uses English name if no Spanish present" do
+        operation = create(:operation,
+          :procedure => create(
+            :procedure, :name_en => "derp"))
+        decorator = V1::PatientCaseDecorator.new(
+          create(:patient_case, :operation => operation))
+
+        decorator.procedure.should == "derp"
+      end
+
+      it "is 'unspecified' if operation has no procedure" do
+        operation = create(:operation, :procedure => nil)
+        decorator = V1::PatientCaseDecorator.new(
+          build(:patient_case, :operation => operation)
+        )
+
+        decorator.procedure.should == "[Operación no especificada]"
+      end
+    end
+  end
+
+  describe "#operation_date" do
+    it "is nil if no operation present" do
+      decorator = V1::PatientCaseDecorator.new(build(:patient_case))
+
+      decorator.operation_date.should be_nil
+    end
+
+    context "in English" do
+      before(:each) do
+        I18n.locale = :en
+      end
+
+      it "outputs date in M/D/Y format" do
+        operation = create(:operation, :date => Date.parse("1975-05-28"))
+        decorator = V1::PatientCaseDecorator.new(
+          create(:patient_case, :operation => operation))
+
+        decorator.operation_date.should == "05/28/1975"
+      end
+
+      it "is 'unspecified' if operation has no date" do
+        operation = create(:operation)
+        decorator = V1::PatientCaseDecorator.new(
+          build(:patient_case, :operation => operation)
+        )
+
+        decorator.operation_date.should == "[Date not specified]"
+      end
+    end
+
+    context "in Spanish" do
+      before(:each) do
+        I18n.locale = :es
+      end
+
+      it "outputs date in D-M-Y format" do
+        operation = create(:operation, :date => Date.parse("1975-05-28"))
+        decorator = V1::PatientCaseDecorator.new(
+          create(:patient_case, :operation => operation))
+
+        decorator.operation_date.should == "28-05-1975"
+      end
+
+      it "is 'unspecified' if operation has no date" do
+        operation = create(:operation)
+        decorator = V1::PatientCaseDecorator.new(
+          build(:patient_case, :operation => operation)
+        )
+
+        decorator.operation_date.should == "[Fecha no especificada]"
+      end
+    end
+  end
+
+  describe "#xrays" do
+    it "returns array of all case xrays by default" do
+      patient_case = create(:patient_case)
+      x1 = create(:pre_op_xray, :patient_case => patient_case)
+      x2 = create(:post_op_xray, :patient_case => patient_case)
+
+      decorator = V1::PatientCaseDecorator.new(patient_case)
+
+      decorator.xrays.should =~ [x1, x2]
+    end
+
+    it "returns only primary x-rays if argument present" do
+      patient_case = create(:patient_case)
+      x1 = create(:pre_op_xray, :patient_case => patient_case)
+      x2 = create(:pre_op_xray, :patient_case => patient_case, :primary => true)
+      x3 = create(:pre_op_xray, :patient_case => patient_case)
+
+      decorator = V1::PatientCaseDecorator.new(patient_case)
+
+      decorator.xrays(:primary => true).should == [x2]
+    end
+
+    it "returns only pre-op x-rays if argument present" do
+      patient_case = create(:patient_case)
+      x1 = create(:pre_op_xray, :patient_case => patient_case)
+      x2 = create(:pre_op_xray, :patient_case => patient_case, :operation_id => 4)
+      x3 = create(:pre_op_xray, :patient_case => patient_case)
+
+      decorator = V1::PatientCaseDecorator.new(patient_case)
+
+      decorator.xrays(:pre_op => true).should =~ [x1, x3]
+    end
+
+    it "returns only post-op x-rays if argument present" do
+      patient_case = create(:patient_case)
+      x1 = create(:pre_op_xray, :patient_case => patient_case)
+      x2 = create(:pre_op_xray, :patient_case => patient_case, :operation_id => 4)
+      x3 = create(:pre_op_xray, :patient_case => patient_case)
+
+      decorator = V1::PatientCaseDecorator.new(patient_case)
+
+      decorator.xrays(:post_op => true).should =~ [x2]
     end
   end
 
